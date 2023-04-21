7d9e99ab11c902c096d03e907396c3de64c3ed9a
Santiago Arango Gutierrez <santiago.arango@experimentality.co>
2023-04-21 14:36:59 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todo_list_app_whith_local_storage';
+}
